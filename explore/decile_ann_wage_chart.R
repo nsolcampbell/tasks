3ef6255141b510875511c007cb1f7d313e314436
Alex Cooper <alex@kuperov.com>
2013-06-06 14:07:19 +1000

    Presentation progress.

--- explore/decile_ann_wage_chart.R
+++ explore/decile_ann_wage_chart.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 
-deciles <- read.csv("deciles_ann_wage_growth.csv", skip=3)
+deciles <- read.csv("../data/deciles_ann_wage_growth.csv", skip=3)
 
 # exclude 2007-2009 (wrong)
 deciles <- deciles[deciles$Period != '2007 to 2009',]
@@ -9,12 +9,13 @@ deciles$Percentile <- as.factor(deciles$Percentile)
 # Patterns of income growth by decile and period, Australia, 1981-82 to 2007-08 
 # Average annual percentage change in real equivalent income unit income, working age
 
-pdf("figure_wage_deciles.pdf", height=8, width=12)
+pdf("../doc/slides/figure_wage_deciles.pdf", height=8, width=12)
 ggplot(deciles, aes(y=Change*100, x=Percentile, group=Period)) + 
   geom_bar(stat="identity") +
   facet_wrap(~Period) +
 #  ggtitle("Wage Changes by Decile") +
   ylab("Percentage change") +
   xlab("Decile") +
-  theme_bw(base_size=18)
+  theme_bw(base_size=32) +
+  theme(strip.background = theme_rect(fill = 'white'))
 dev.off()
