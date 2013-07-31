3565219fa4032c66299032d8344bce5b51a0b6a6
Alex Cooper <alex@acooper.org>
2013-07-31 10:44:36 +1000

    Occupational share charts

--- figure/occ_share.R
+++ figure/occ_share.R
@@ -26,26 +26,72 @@ open_supertable_file <- function(filename) {
 	return(datatable)
 }
 
-combined <- open_supertable_file("data/ft_occ_emp_quarter.txt")
-p <- ggplot(combined, aes(x=quarter, y=prop, group=occupation, color=occupation)) + 
-				geom_line() +
-				scale_y_continuous(labels=percent) +
-				ylab("Share of Employment") +
-				xlab("Quarter") +
-				ggtitle("Employment Share by Broad Occupational Group") +
-				theme(legend.position='right')
+condense <- function(tenocc) {
+	require(doBy)
+	require(reshape2)
+	short_data <- dcast(tenocc, quarter ~ occupation, value.var='prop')
+	names(short_data) <- make.names(names(short_data))
+	condensed_data <- with(short_data, data.frame(quarter, 
+					manprof=Managers+Professionals+Technicians.and.Trades.Workers,
+					cleric =Clerical.and.Administrative.Workers+Sales.Workers,
+					produc =Machinery.Operators.And.Drivers+Labourers,
+					service=Community.and.Personal.Service.Workers))
+	datatable <- melt(condensed_data, id.vars="quarter")
+	names(datatable) <- c("quarter", "occupation", "prop")
+	datatable$occupation <- recodeVar(datatable$occupation,
+																		c("manprof","cleric","produc","service"),
+																		c("Professional, Managerial, Technical", 
+																			"Clerical, Sales",
+																			"Production, Operators",
+																			"Service"))
+	datatable$occupation <- 
+		factor(datatable$occupation, 
+					 levels=c("Professional, Managerial, Technical", 
+					 				 "Clerical, Sales",
+					 				 "Production, Operators",
+					 				 "Service"),
+					 ordered=T)
+	return(datatable)
+}
 
-# Compare males, females
-
-male <- open_supertable_file("data/ft_occ_emp_quarter_male.txt")
-male$sex <- "Male"
-female <- open_supertable_file("data/ft_occ_emp_quarter_female.txt")
-female$sex <- "Female"
-malefemale <- rbind(male, female)
-
-pdf("figure/occ_share.pdf", paper="a4r", height=8, width=12)
-	p
+occupation_share_charts <- function() {
+	combined <- open_supertable_file("data/ft_occ_emp_quarter.txt")
+	combined.c <- condense(combined)
+	
+	p <- ggplot(combined, aes(x=quarter, y=prop, group=occupation, color=occupation)) + 
+					geom_line() +
+					scale_y_continuous(labels=percent) +
+					ylab("Share of Employment") +
+					xlab("Quarter") +
+					ggtitle("Employment Share by Broad Occupational Group") +
+					theme(legend.position='right')
+	
+	# Compare males, females
+	
+	male <- open_supertable_file("data/ft_occ_emp_quarter_male.txt")
+	male.c <- condense(male)
+	male$sex <- "Male"; male.c$sex <- "Male"
+	female <- open_supertable_file("data/ft_occ_emp_quarter_female.txt")
+	female.c <- condense(female)
+	female$sex <- "Female"; female.c$sex <- "Female"
+	malefemale <- rbind(male, female)
+	malefemale.c <- rbind(male.c, female.c)
+	
+	print(p)
 	fn()
-	p %+% malefemale + facet_grid(.~sex)
+	print(p %+% combined.c +
+		ggtitle("Employment Share by Major Occupational Group"))
 	fn()
-dev.off()
+	print(p %+% malefemale + facet_grid(sex~.) +
+		ggtitle("Employment Share by Broad Occupational Group, by Sex"))
+	fn()
+	print(p %+% malefemale.c + facet_grid(sex~.)  +
+		ggtitle("Employment Share by Major Occupational Group, by Sex"))
+	fn()
+}
+
+occupation_share_charts_pdf <- function(filename="figure/occ_share.pdf") {
+	pdf(filename, paper="a4r", height=8, width=12)
+	occupation_share_charts()
+	dev.off()
+}
