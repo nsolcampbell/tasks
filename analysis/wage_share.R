68ceded3ab45a55fa42e0a0242332bbb220cb376
Alex Cooper <alex@kuperov.com>
2013-08-11 23:43:49 +1000

    Fixed coding bug. Split into two periods for identification.

--- analysis/wage_share.R
+++ analysis/wage_share.R
@@ -1,4 +1,10 @@
+rm(list=ls())
+
 require(plyr)
+library('doBy')
+library('reshape2')
+library(ggplot2)
+source('lib/plot_theme.R')
 
 combined <- read.csv("data/5204.0/combined.csv")
 rownames(combined) <- gsub(" $","", combined$Industry, perl=T)
@@ -6,22 +12,22 @@ combined <- combined[,-1]
 
 # these rows get merged into one 
 combrows <- c("Rental, hiring and real estate services",
-                  "Professional, scientific and technical services",
-                  "Administrative and support services")
+              "Professional, scientific and technical services",
+              "Administrative and support services")
 combrows_idxs <- which(rownames(combined) %in% combrows)
 combined["Property and Business Services",] <-
-    colSums(combined[combrows_idxs,])
+  colSums(combined[combrows_idxs,])
 combined <- combined[-combrows_idxs,]
 # these guys get removed because inconsistently coded
 droprows <- c("Other services")
 combined <- combined[-which(rownames(combined) %in% droprows),]
 
 mysubset <- function(regex) {
-    s <- combined[,grep(regex, colnames(combined))]
-    s <- s[,(ncol(s)-22):ncol(s)]
-    rownames(s) <- combined$Industry
-    colnames(s) <- paste("Y",1990:2012, sep="")
-    return(s)
+  s <- combined[,grep(regex, colnames(combined))]
+  s <- s[,(ncol(s)-22):ncol(s)]
+  rownames(s) <- combined$Industry
+  colnames(s) <- paste("Y",1990:2012, sep="")
+  return(s)
 }
 
 valadd <- mysubset("^GVA")
@@ -49,12 +55,12 @@ mysubset$Group <- recodeVar(as.character(mysubset$COccupA),
                             c("Nonroutine Manual","Routine","Nonroutine Nonmanual"),
                             c("L","M","H"))
 
-mysubset <- subset(mysubset, Year %in% c("1995", "2010"))
+mysubset <- subset(mysubset, Year %in% c("1996", "2003", "2010"))
 
 # wage bill: Year/Group (scale by person weight)
 ye_wb <- ddply(mysubset, .(Year, Group, CIndA), summarise, 
-                  mean_wage=weighted.mean(CInc, Weight),
-                  wage_bill=sum(Weight * CInc))
+               mean_wage=weighted.mean(CInc, Weight),
+               wage_bill=sum(Weight * CInc))
 # wage bill: Year
 y_wb <- ddply(mysubset, .(Year, CIndA), summarise,
               year_wage_bill=sum(Weight * CInc))
@@ -69,29 +75,63 @@ wage_arr  <- acast(wb, Year ~ CIndA ~ Group, value.var="mean_wage")
 HL <- wage_arr[,,"H"] / wage_arr[,,"L"]
 ML <- wage_arr[,,"M"] / wage_arr[,,"L"]
 
-# difference in wage share
-d_share <- share_arr["2010",,] - share_arr["1995",,]
-colnames(d_share) <- c("dshare_L", "dshare_M", "dshare_H")
-
+d_share9603 <- data.frame(share_arr["2003",,] - share_arr["1996",,])
+d_share9603$Year <- 2003
 # assemble regression data frame
-regdata <- data.frame(cbind(
-    d_share=d_share, # wage bill share
-    dlog_HL=log(HL["2010",]) - log(HL["1995",]), # difference in log H/L ratio
-    dlog_ML=log(ML["2010",]) - log(ML["1995",]), # difference in log M/L ratio
-    d_HL=HL["2010",] - HL["1995",], # difference in H/L ratio
-    d_ML=ML["2010",] - ML["1995",], # difference in M/L ratio
-    dl_nonict=with(l_nonict, Y2010-Y1995), # dlog of non-ICT capital
-    dl_ict   =with(l_ict,    Y2010-Y1995), # dlog of ICT capital
-    dl_periph=with(l_periph, Y2010-Y1995), # dlog of ICT hardware capital
-    dl_soft  =with(l_soft,   Y2010-Y1995), # dlog of ICT software capital
-    dl_equip =with(l_equip,    Y2010-Y1995), # dlog of ICT equipment capital
-    dl_valadd=with(l_valadd, Y2010-Y1995)  # dlog of value added
+regdata1 <- data.frame(cbind(
+  d_share9603,
+  ind=dimnames(share_arr)[[2]],
+  dlog_HL=log(HL["2003",]) - log(HL["1996",]), # difference in log H/L ratio
+  dlog_ML=log(ML["2003",]) - log(ML["1996",]), # difference in log M/L ratio
+  d_HL=HL["2003",] - HL["1996",], # difference in H/L ratio
+  d_ML=ML["2003",] - ML["1996",], # difference in M/L ratio
+  dl_nonict=with(l_nonict, Y2003-Y1996), # dlog of non-ICT capital
+  dl_ict   =with(l_ict,    Y2003-Y1996), # dlog of ICT capital
+  dl_periph=with(l_periph, Y2003-Y1996), # dlog of ICT hardware capital
+  dl_soft  =with(l_soft,   Y2003-Y1996), # dlog of ICT software capital
+  dl_equip =with(l_equip,  Y2003-Y1996), # dlog of ICT equipment capital
+  dl_valadd=with(l_valadd, Y2003-Y1996)  # dlog of value added
 ))
-regdata$ind=as.factor(rownames(regdata))
 
-rd_long <- melt(regdata, measure=c("dshare_L", "dshare_M", "dshare_H"),variable.name="group",value.name="dshare")
-rd_long$group <- as.factor(recodeVar(as.character(rd_long$group), c("dshare_L", "dshare_M", "dshare_H"), c("L","M","H")))
-rd_long$ind <- factor(rd_long$ind)
+d_share0310 <- data.frame(share_arr["2010",,] - share_arr["2003",,])
+d_share0310$Year <- 2010
+# assemble regression data frame
+regdata2 <- data.frame(cbind(
+  d_share0310,
+  ind=dimnames(share_arr)[[2]],
+  dlog_HL=log(HL["2010",]) - log(HL["2003",]), # difference in log H/L ratio
+  dlog_ML=log(ML["2010",]) - log(ML["2003",]), # difference in log M/L ratio
+  d_HL=HL["2010",] - HL["2003",], # difference in H/L ratio
+  d_ML=ML["2010",] - ML["2003",], # difference in M/L ratio
+  dl_nonict=with(l_nonict, Y2010-Y2003), # dlog of non-ICT capital
+  dl_ict   =with(l_ict,    Y2010-Y2003), # dlog of ICT capital
+  dl_periph=with(l_periph, Y2010-Y2003), # dlog of ICT hardware capital
+  dl_soft  =with(l_soft,   Y2010-Y2003), # dlog of ICT software capital
+  dl_equip =with(l_equip,  Y2010-Y2003), # dlog of ICT equipment capital
+  dl_valadd=with(l_valadd, Y2010-Y2003)  # dlog of value added
+))
+
+d_shares <- rbind(regdata1, regdata2)
+colnames(d_shares) <- colnames(regdata1)
+
+rd_long <- melt(d_shares, measure=c("L", "M", "H"),variable.name="group",value.name="dshare")
+
+rd_long <- subset(rd_long, ind != 'Accommodation and Food Services') # badly coded
+
+rd_long$group <- with(rd_long, recodeVar(as.character(group),
+                                         c("L","M","H"),
+                                         c("Low Skill", "Medium Skill", "High Skill")))
+rd_long$group <- with(rd_long, factor(group, ordered=T,
+                                      levels=c("Low Skill", "Medium Skill", "High Skill")))
+
+summary(lm(dshare ~ dl_nonict + dl_ict + dl_valadd, data=rd_long))
+
+qplot(dl_equip, dshare, color=group, fill=group, data=subset(rd_long, T==T)) + 
+    geom_smooth(method="lm", alpha=0.25) + facet_grid(.~group) + 
+    ggtitle("Change in Wage Share and ICT Equipment Investment 1996-2010") +
+    xlab("Change in log equipment capital-value added ratio") +
+    ylab("Change in wage share") +
+    theme(legend.position="none")
 
 summary(lm(dshare_H ~ dlog_HL + dlog_ML + dl_nonict + dl_ict + dl_valadd, data=regdata))
 summary(lm(dshare_M ~ dlog_HL + dlog_ML + dl_nonict + dl_ict + dl_valadd, data=regdata))
@@ -112,4 +152,3 @@ summary(lm(dshare_L ~ dl_nonict + dl_ict, data=regdata))
 # Does ICT investment even affect the differential? Nope.
 summary(lm(d_HL ~ dl_nonict + dl_ict + dl_valadd, data=regdata))
 summary(lm(d_ML ~ dl_nonict + dl_ict + dl_valadd, data=regdata))
-
