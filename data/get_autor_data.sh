2e2d02945079655f18592d3ce614552703dc8bea
Alex Cooper <alex@kuperov.com>
2013-08-12 11:46:49 +1000

    script to retrieve David Autor's data

---
+++ data/get_autor_data.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# This downloads the data that we use from David Autor's web site
+
+mkdir autor
+cd autor
+wget http://economics.mit.edu/~dautor/hole-vol4/figs/fig-07.zip
+unzip fig-07.zip
+
