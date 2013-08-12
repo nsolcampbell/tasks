7d56f53200c6475554235d2dd69ab6254fb17f63
Alex Cooper <alex@kuperov.com>
2013-08-12 11:37:21 +1000

    updated coding and theme for paper

--- lib/curfmunge.R
+++ lib/curfmunge.R
@@ -81,10 +81,80 @@ recodeAge4 <- function(age) {
 
 lf_status <- c("Full-time", "Part-time", "Other")
 recodeFullTime <- function(status) {
-    recodeVar(as.character(subs$LFSTBCP),
+    recodeVar(as.character(status),
               list(c("Employed full-time"), 
                    c("Employed part-time"), 
                    c("Unemployed", "Not in the labour force")),
               as.list(lf_status))
 }
 
+indA_list <- c("Agriculture, Forestry and Fishing", 
+               "Mining", 
+               "Manufacturing", 
+               "Electricity, Gas, Water and Waste Services", 
+               "Construction", 
+               "Wholesale Trade", 
+               "Retail Trade", 
+               "Accommodation and Food Services", 
+               "Transport, Postal and Warehousing", 
+               "Information Media and Telecommunications", 
+               "Financial and Insurance Services", 
+               "Property and Business Services", 
+               "Public Administration and Safety", 
+               "Education and Training", 
+               "Health Care and Social Assistance", 
+               "Arts and Recreation Services", 
+               "Other Services", 
+               "Unknown")
+recode_IndA <- function(ind) {
+    require(doBy)
+    recodeVar(as.character(ind),
+              list(c("Agriculture, Forestry and Fishing", "Agriculture, forestry and fishing", "Agriculture,forestry, fishing and hunting"), 
+                    c("Mining"), 
+                    c("Manufacturing"), 
+                    c("Electricity, Gas, Water and Waste Services", "Electricity, gas and water", "Electricity, gas and water supply",
+                      "Electricity, Gas and Water Supply"), 
+                    c("Construction"), 
+                    c("Wholesale Trade", "Wholesale trade"), 
+                    c("Retail Trade", "Retail trade"), 
+                    c("Accommodation and Food Services", "Accommodation, cafes and restaurants", "Accommodation,cafes and restaurants",
+                      "Accomodation, Cafes and Restaurants", "Accommodation, Cafes and Restaurants"), 
+                    c("Transport, Postal and Warehousing", "Transport and storage", "Transport and Storage"), 
+                    c("Information Media and Telecommunications", "Communication Services", "Communication services"), 
+                    c("Financial and Insurance Services", "Finance and insurance", "Finance and Insurance"), 
+                    c("Rental, Hiring and Real Estate Services", 
+                      "Professional, Scientific and Technical Services", 
+                      "Property and business services", "Property and business services",
+                      "Administrative and Support Services"),
+                    c("Public Administration and Safety", "Government administration and defence", "Government Administration and Ddefence",
+                      "Government Administration and Defence"), 
+                    c("Education and Training", "Education"), 
+                    c("Health Care and Social Assistance", "Health and community services", "Health and Community Services"), 
+                    c("Arts and Recreation Services", "Cultural and Recreational Sservices", "Cultural and recreational services",
+                      "Cultural and Recreational Services"), 
+                    c("Other Services", "Personal and Other Services", "Personal and other services"), 
+                    c("Inadequately described", "Not applicable", "Invalid", "Not applicable and inadequately described", "Unknown")),
+              as.list(indA_list)
+              )
+}
+
+occupA_list <- c("Nonroutine Nonmanual", "Routine", "Nonroutine Manual","Other")
+recode_occupA <- function(occ) {
+    recodeVar(as.character(occ),
+              list(c("Professionals", "Managers and Administrators", "Managers and administrators"),
+                   c("Para-professionals","Para-professionals","Clerks","Salespersons and personal service workers",
+                     "Associate professionals","Intermediate clerical, sales and service workers",
+                     "Elementary clerical, sales and service workers","Associate Professionals",
+                     "Elementary Clerical, Sales and Service Workers","Sales Workers","Clerical and Administrative Workers",
+                     "Sales Workers","Intermediate Clerical, Sales and Service Workers","Advanced Clerical and Service Workers",
+                     "Advanced clerical and service workers"),
+                   c("Tradespersons","Defence forces","Plant and machine operators and drivers",
+                     "Intermediate production and transport workers","Tradespersons and Related Workers",
+                     "Intermediate Production and Transport Workers","Labourers and Related Workers",
+                     "Technicians and Trade Workers","Machinery Operators and Drivers","Labourers","Tradespersons and related workers",
+                     "Plant and machine operators, and drivers","Labourers and related workers","Community and Personal Service Workers"),
+                   c("Inadequately described","Other", "Not applicable")
+                  ),
+              as.list(occupA_list)
+    )
+}
--- lib/plot_theme.R
+++ lib/plot_theme.R
@@ -3,7 +3,7 @@ suppressMessages(require(grid))
 # A consistent, minimal theme for all plots
 t <- theme_bw(15) +
     theme(plot.margin=unit(c(1,1,1,1),"cm"), 
-          plot.title=element_text(size=18, vjust=3),
+          plot.title=element_text(size=18, vjust=2),
           axis.title.x=element_text(size=15, vjust=-0.5),
           axis.title.y=element_text(size=15, vjust=-0.25),
           strip.text=element_text(size=15),
