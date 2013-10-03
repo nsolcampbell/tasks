05f6650977ee6f859daaaf13eb44694a1ac22f7d
Alex Cooper <alex@kuperov.com>
2013-10-03 21:31:10 +1000

    Weighted O*NET files for merging in STATA

---
+++ munge/55_generate_weighted_merge_files.R
@@ -0,0 +1,39 @@
+rm(list=ls())
+# this script creates reference data for STATA implementations of RIF regressions
+
+# we take the weighted task measures for each grouping scheme, 
+# and then do a cartesian product with the ANZSCO mapping
+
+# COMBINED 1 mapping
+load("data/tasks.combinedi.rda")
+# map to CCLO (1981-82)
+map.i <- read.csv("data/mappings/cclo_combinedi.csv")
+merged.i <- merge(x=tasks.combinedi, y=map.i, by="COMBINED1")
+cols.i <- c("CCLO", "COMBINED1", "Information.Content", "Automation.Routinization", 
+            "Face.to.Face", "On.Site.Job", "Decision.Making", "Population", 
+            "Title", "COMBINED1.Title")
+write.csv(merged.i[,cols.i], file="data/mappings/tasks_mapped_cclo_i.csv")
+
+map.i <- read.csv("data/mappings/anzsco_combinedi.csv")
+merged.i <- merge(x=tasks.combinedi, y=map.i, by="COMBINED1")
+cols.i <- c("ANZSCO", "COMBINED1", "Information.Content", "Automation.Routinization",
+          "Face.to.Face", "On.Site.Job", "Decision.Making", "Population", "Title")
+write.csv(merged.i[,cols.i], file="data/mappings/tasks_mapped_anzsco_i.csv")
+
+# ### COMBINED 1 mapping ###
+
+load("data/tasks.combinedii.rda")
+# map to ASCOII (2000-1)
+map.ii <- read.csv("data/mappings/ascoii_combinedii.csv")
+merged.ii <- merge(map.ii, tasks.combinedii)
+cols.ii <- c("ASCO2", "COMBINEDII", "COMBINEDII.Name", "Information.Content", 
+             "Automation.Routinization", "Face.to.Face", "On.Site.Job", "Decision.Making", 
+             "Population", "CombinedII.Title")
+write.csv(merged.i[,cols.i], file="data/mappings/tasks_mapped_ascoii_ii.csv")
+# map to ANZSCO (2009-10)
+# and overwrite existing variables
+map.ii <- read.csv("data/mappings/anzsco_combinedii.csv")
+merged.ii <- merge(x=tasks.combinedi, y=map.i, by="COMBINED1")
+cols.ii <- c("ANZSCO", "COMBINEDII", "Information.Content", "Automation.Routinization",
+             "Face.to.Face", "On.Site.Job", "Decision.Making", "Population", "Title")
+write.csv(merged.i[,cols.i], file="data/mappings/tasks_mapped_anzsco_ii.csv")
